1aab5be149ed82046c19c480670d49349856a8b6
Александр <90699299+AlexanderFix@users.noreply.github.com>
2025-05-15 17:38:52 +0300

    .

--- js/index.js
+++ js/index.js
@@ -23,3 +23,52 @@ burgerList.forEach(item => {
 burger_button.addEventListener('click', menuBurger)
 
 //Burger end
+
+// Menu select start
+const selectSingle = document.querySelector('.form__select');
+const selectSingle_title = selectSingle.querySelector('.form__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.form__select__label');
+const selectSingle_input = document.getElementsByClassName('form__select__input');
+const selectSingle_content = document.querySelector('.form__select__content')
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+    if (selectSingle_content.style.display == 'flex') {
+        selectSingle_content.style.display = 'none'
+    } else {
+        selectSingle_content.style.display = 'flex'
+    }
+
+    if ('active' === selectSingle.getAttribute('data-state')) {
+        selectSingle.setAttribute('data-state', '');
+
+    } else {
+        selectSingle.setAttribute('data-state', 'active');
+    }
+
+
+
+
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+
+
+
+    selectSingle_labels[i].addEventListener('click', (evt) => {
+        selectSingle_title.textContent = evt.target.textContent;
+        selectSingle.setAttribute('data-state', '',);
+
+        selectSingle_content.style.display = 'none';
+
+    });
+
+    
+
+}
+
+
+// Menu select end
+
+
